b8e6cbacbaf2519d2c554726689d60d667f61025
Danielalab <olabolacrayola@hotmail.com>
2020-07-23 17:23:33 -0500

    feature(index.js): add click btn sumar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import cipher from './cipher.js';
+import mathOperations from './math-operations.js';
+/* import { multiplicar, obj3 } from './math-operations'; */
 
-console.log(cipher);
+console.log(mathOperations);
+
+
+const btnSumar = document.getElementById('btn-sumar');
+const inputNumberOne = document.getElementById('numb-one');
+const inputNumberTwo = document.getElementById('numb-two');
+const elementRespuesta = document.getElementById('respuesta');
+
+function onClickBtnSumar() {
+  const valueNumbOne = parseInt(inputNumberOne.value);
+  const valueNumbTwo = parseInt(inputNumberTwo.value);
+
+  console.log(valueNumbOne, valueNumbTwo);
+
+  const sumaTotal = mathOperations.sumar(valueNumbOne, valueNumbTwo);
+  elementRespuesta.innerHTML = `<p>Rsultado final : ${sumaTotal} </p>`;
+/*   elementRespuesta.classList.add('respuesta'); */
+  elementRespuesta.textContent = sumaTotal;
+}
+
+btnSumar.addEventListener('click', onClickBtnSumar);
+
