50a7081bfd7699dc7f33fb8b8130e8501d3f889e
Danielalab <olabolacrayola@hotmail.com>
2020-07-23 17:21:52 -0500

    feature(matg-operations): add fn sumar & fn restar

---
+++ src/math-operations.js
@@ -0,0 +1,40 @@
+const mathOperations = {
+  sumar: (numb1 , numb2) => {
+    const result = numb1 + numb2;
+    return result;
+  },
+  restar: (numb1, numb2) => {
+    const result = numb1 - numb2;
+    return result;
+  }
+};
+
+
+export default mathOperations;
+
+
+
+/* const obj2 = {}
+
+export const obj3 = {};
+
+
+export const multiplicar = () => {}; */
+
+/* const mathOperations2 = {};
+
+mathOperations2.sumar = (numb1 , numb2) => {
+  const result = numb1 + numb2;
+  return result;
+};
+ */
+
+// necesito los dos numeros
+// sumar los dos numeros
+// retornar la suma
+/* 
+const mascota = {
+  name: 'Dupin',
+  edad: 2,
+  comer: () => {}
+} */
